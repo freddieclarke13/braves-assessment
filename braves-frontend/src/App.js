29edff0dc30f3fd6d386b7bf383865988bed8116
Freddie Clarke <freddieclarke13@gmail.com>
2023-11-13 18:33:04 -0500

    pushing code to remote

--- braves-frontend/src/App.js
+++ braves-frontend/src/App.js
@@ -1,16 +1,316 @@
-import "./App.css";
-import ReactPlayer from "react-player";
-import Typography from "@mui/material/Typography";
-import { Card, CardActionArea, CardMedia, Grid } from "@mui/material";
+import React from "react";
+import axios from "axios";
+import {
+  Grid,
+  Typography,
+  Card,
+  CardActionArea,
+  CardMedia,
+  Button,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material";
 
-function App() {
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Montserrat", "sans-serif"].join(","),
+  },
+});
+
+const App = () => {
+  const [predictions, setPredictions] = React.useState([]);
+  const [currentPlay, setCurrentPlay] = React.useState(0);
+
+  React.useEffect(() => {
+    axios
+      .get("http://127.0.0.1:5000/predictions")
+      .then((response) => {
+        setPredictions(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const handlePrevClick = () => {
+    if (currentPlay > 0) {
+      setCurrentPlay(currentPlay - 1);
+    }
+  };
+  const handleNextClick = () => {
+    if (currentPlay < predictions.length - 1) {
+      setCurrentPlay(currentPlay + 1);
+    }
+  };
+
+  const renderPlay = () => {
+    if (predictions.length == 0) {
+      return <div>Loading</div>;
+    }
+
+    const play = predictions[currentPlay];
+
+    return (
+        <div className="prediction">
+          <Grid container spacing={2}>
+            <Grid item xs={3} sm={3} md={3} lg={3} xl={3}></Grid>
+            <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
+              <Card
+                sx={{
+                  marginTop: 4,
+                  padding: 4,
+                  bgcolor: "#13274F",
+                  color: "#ffffff",
+                }}
+              >
+                <Grid
+                  container
+                  spacing={2}
+                  align="center"
+                  justify="center"
+                  alignItems="center"
+                >
+                  <Grid item xs={2} sm={2} md={2} lg={2} xl={2}></Grid>
+                  <Grid
+                    item
+                    xs={8}
+                    sm={8}
+                    md={8}
+                    lg={8}
+                    xl={8}
+                    sx={{
+                      justifyContent: "center",
+                      alignItems: "center",
+                      textAlign: "center",
+                    }}
+                  >
+                    <CardActionArea>
+                      <CardMedia
+                        component="iframe"
+                        title="test"
+                        src={play.videoLink}
+                        sx={{ width: 500, height: 281 }}
+                      />
+                    </CardActionArea>
+                  </Grid>
+                  <Grid item xs={2} sm={2} md={2} lg={2} xl={2}></Grid>
+                  <Grid item xs={3} sm={3} md={3} lg={3} xl={3}></Grid>
+                  <Grid
+                    item
+                    xs={6}
+                    sm={6}
+                    md={6}
+                    lg={6}
+                    xl={6}
+                    sx={{
+                      justifyContent: "center",
+                      alignItems: "center",
+                      textAlign: "center",
+                    }}
+                  >
+                    <Typography>
+                      <span style={{ color: "#CE1141" }}>
+                        <b>Game Date:</b>
+                      </span>{" "}
+                      {play.date}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={3} sm={3} md={3} lg={3} xl={3}></Grid>
+                  <Grid item xs={2} sm={2} md={2} lg={2} xl={2}></Grid>
+                  <Grid
+                    item
+                    xs={4}
+                    sm={4}
+                    md={4}
+                    lg={4}
+                    xl={4}
+                    sx={{
+                      justifyContent: "center",
+                      alignItems: "center",
+                      textAlign: "center",
+                    }}
+                  >
+                    <Typography>
+                      <span style={{ color: "#CE1141" }}>
+                        <b>True Outcome:</b>
+                      </span>{" "}
+                      {play.trueOutcome}
+                    </Typography>
+                  </Grid>
+                  <Grid
+                    item
+                    xs={4}
+                    sm={4}
+                    md={4}
+                    lg={4}
+                    xl={4}
+                    sx={{
+                      justifyContent: "center",
+                      alignItems: "center",
+                      textAlign: "center",
+                    }}
+                  >
+                    <Typography>
+                      <span style={{ color: "#CE1141" }}>
+                        <b>Predicted Outcome:</b>
+                      </span>{" "}
+                      {play.predictedOutcome}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={2} sm={2} md={2} lg={2} xl={2}></Grid>
+                  <Grid item xs={2} sm={2} md={2} lg={2} xl={2}></Grid>
+                  <Grid
+                    item
+                    xs={4}
+                    sm={4}
+                    md={4}
+                    lg={4}
+                    xl={4}
+                    sx={{
+                      justifyContent: "center",
+                      alignItems: "center",
+                      textAlign: "center",
+                    }}
+                  >
+                    <Typography>
+                      <span style={{ color: "#CE1141" }}>
+                        <b>Batter:</b>
+                      </span>{" "}
+                      {play.batter}
+                    </Typography>
+                  </Grid>
+                  <Grid
+                    item
+                    xs={4}
+                    sm={4}
+                    md={4}
+                    lg={4}
+                    xl={4}
+                    sx={{
+                      justifyContent: "center",
+                      alignItems: "center",
+                      textAlign: "center",
+                    }}
+                  >
+                    <Typography>
+                      <span style={{ color: "#CE1141" }}>
+                        <b>Pitcher:</b>
+                      </span>{" "}
+                      {play.pitcher}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={2} sm={2} md={2} lg={2} xl={2}></Grid>
+                  <Grid item xs={2} sm={2} md={2} lg={2} xl={2}></Grid>
+                  <Grid
+                    item
+                    xs={4}
+                    sm={4}
+                    md={4}
+                    lg={4}
+                    xl={4}
+                    sx={{
+                      justifyContent: "center",
+                      alignItems: "center",
+                      textAlign: "center",
+                    }}
+                  >
+                    <Typography>
+                      <span style={{ color: "#CE1141" }}>
+                        <b>Launch Angle:</b>
+                      </span>{" "}
+                      {play.launchAngle}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+                    <Typography>
+                      <span style={{ color: "#CE1141" }}>
+                        <b>Exit Speed:</b>
+                      </span>{" "}
+                      {play.exitSpeed}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={2} sm={2} md={2} lg={2} xl={2}></Grid>
+                  <Grid item xs={2} sm={2} md={2} lg={2} xl={2}></Grid>
+                  <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+                    <Typography>
+                      <span style={{ color: "#CE1141" }}>
+                        <b>Exit Direction:</b>
+                      </span>{" "}
+                      {play.exitDirection}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+                    <Typography>
+                      <span style={{ color: "#CE1141" }}>
+                        <b>Hit Distance:</b>
+                      </span>{" "}
+                      {play.hitDistance}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={2} sm={2} md={2} lg={2} xl={2}></Grid>
+                  <Grid item xs={2} sm={2} md={2} lg={2} xl={2}></Grid>
+                  <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+                    <Typography>
+                      <span style={{ color: "#CE1141" }}>
+                        <b>Hang Time:</b>
+                      </span>{" "}
+                      {play.hangTime}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+                    <Typography>
+                      <span style={{ color: "#CE1141" }}>
+                        <b>Spin Rate:</b>
+                      </span>{" "}
+                      {play.hitSpinRate}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={2} sm={2} md={2} lg={2} xl={2}></Grid>
+                  <Grid item xs={4} sm={4} md={4} lg={4} xl={4}></Grid>
+                  <Grid item xs={2} sm={2} md={2} lg={2} xl={2}>
+                    <Button
+                      variant="contained"
+                      onClick={handlePrevClick}
+                      disabled={currentPlay === 0}
+                      sx={{ backgroundColor: "#CE1141" }}
+                    >
+                      Previous
+                    </Button>
+                  </Grid>
+                  <Grid item xs={2} sm={2} md={2} lg={2} xl={2}>
+                    <Button
+                      variant="contained"
+                      onClick={handleNextClick}
+                      disabled={currentPlay === predictions.length - 1}
+                      sx={{ backgroundColor: "#CE1141" }}
+                    >
+                      Next
+                    </Button>
+                  </Grid>
+                  <Grid item xs={4} sm={4} md={4} lg={4} xl={4}></Grid>
+                </Grid>
+              </Card>
+            </Grid>
+            <Grid item xs={3} sm={3} md={3} lg={3} xl={3}></Grid>
+          </Grid>
+        </div>
+    );
+  };
+
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
+    <div>
       <Grid container spacing={2} sx={{ bgcolor: "#13274F" }}>
-        <Grid item xs={3} sm={3} md={3} lg={3} xl={3}>
-          <img src="https://1000logos.net/wp-content/uploads/2017/08/Atlanta-Braves-logo.png"></img>
-        </Grid>
-        <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
+        <Grid item xs={3} sm={3} md={3} lg={3} xl={3}></Grid>
+        <Grid
+          item
+          xs={6}
+          sm={6}
+          md={6}
+          lg={6}
+          xl={6}
+          style={{ textAlign: "center" }}
+        >
           <Typography
             sx={{ color: "#CE1141", fontWeight: 500 }}
             variant="h2"
@@ -21,63 +321,116 @@ function App() {
         </Grid>
         <Grid item xs={3} sm={3} md={3} lg={3} xl={3}></Grid>
       </Grid>
-      <Grid container spacing={2} >
-        <Grid item xs={4} sm={4} md={4} lg={4} xl={4}></Grid>
+      <div className="predictions-container">{renderPlay()}</div>
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          bgcolor: "#13274F",
+          marginTop: 4,
+          paddingLeft: 2,
+          paddingRight: 2,
+          paddingBottom: 4,
+        }}
+      >
+        <Grid item xs={3} sm={3} md={3} lg={3} xl={3}></Grid>
+        <Grid
+          item
+          xs={6}
+          sm={6}
+          md={6}
+          lg={6}
+          xl={6}
+          style={{ textAlign: "center" }}
+        >
+          <Typography sx={{ color: "#CE1141", fontWeight: 400 }} variant="h3">
+            Data Overview
+          </Typography>
+        </Grid>
+        <Grid item xs={3} sm={3} md={3} lg={3} xl={3}></Grid>
+        <Grid item xs={2} sm={2} md={2} lg={2} xl={2}></Grid>
         <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
-          <Card sx={{marginTop:4, padding:4, bgcolor: "#13274F", color:'#ffffff' }}>
-            <Grid container spacing={2}>
-            <Grid item xs={4} sm={4} md={4} lg={4} xl={4}></Grid>
-              <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
-                <CardActionArea>
-                  <CardMedia
-                    component="iframe"
-                    title="test"
-                    src="https://coach-video.mlb.com/mlb/2018/04/01/e0429a12/0d38d01c/video_1522621744505_4500k.mp4"
-                  />
-                </CardActionArea>
-              </Grid>
-              <Grid item xs={4} sm={4} md={4} lg={4} xl={4}></Grid>
-              <Grid item xs={3} sm={3} md={3} lg={3} xl={3}></Grid>
-              <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
-                <Typography>Game Date: 4/1/2018</Typography>
-              </Grid>
-              <Grid item xs={3} sm={3} md={3} lg={3} xl={3}></Grid>
-              <Grid item xs={3} sm={3} md={3} lg={3} xl={3}></Grid>
-              <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
-                <Typography>Play Outcome: Out</Typography>
-              </Grid>
-              <Grid item xs={3} sm={3} md={3} lg={3} xl={3}></Grid>
-              <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
-                <Typography>Batter: Yonder Alonso</Typography>
-              </Grid>
-              <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
-                <Typography>Pitcher: Mike Leake</Typography>
-              </Grid>
-              <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
-                <Typography>Launch Angle: 5.1</Typography>
-              </Grid>
-              <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
-                <Typography>Exit Speed: 82.3</Typography>
-              </Grid>
-              <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
-                <Typography>Exit Direction: 38.0</Typography>
-              </Grid>
-              <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
-                <Typography>Hit Distance: 72.9</Typography>
-              </Grid>
-              <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
-                <Typography>Hang Time: 0.7</Typography>
-              </Grid>
-              <Grid item xs={6} sm={6} md={6} lg={6} xl={6}>
-                <Typography>Spin Rate: 4206.2</Typography>
-              </Grid>
-            </Grid>
+          <Card align="center" justify="center" alignItems="center">
+            <CardMedia
+              component="img"
+              height="250"
+              image={require("./exit_speed_distribution.png")}
+              style={{
+                width: "auto",
+              }}
+            ></CardMedia>
           </Card>
         </Grid>
-        <Grid item xs={4} sm={4} md={4} lg={4} xl={4}></Grid>
+        <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+          <Card align="center" justify="center" alignItems="center">
+            <CardMedia
+              component="img"
+              height="250"
+              image={require("./hit_distance_distribution.png")}
+              style={{
+                width: "auto",
+              }}
+            ></CardMedia>
+          </Card>
+        </Grid>
+        <Grid item xs={2} sm={2} md={2} lg={2} xl={2}></Grid>
+        <Grid item xs={2} sm={2} md={2} lg={2} xl={2}></Grid>
+        <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+          <Card align="center" justify="center" alignItems="center">
+            <CardMedia
+              component="img"
+              height="250"
+              image={require("./launch_angle_distribution.png")}
+              style={{
+                width: "auto",
+              }}
+            ></CardMedia>
+          </Card>
+        </Grid>
+        <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+          <Card align="center" justify="center" alignItems="center">
+            <CardMedia
+              component="img"
+              height="250"
+              image={require("./exit_velocity_vs_hit_distance.png")}
+              style={{
+                width: "auto",
+              }}
+            ></CardMedia>
+          </Card>
+        </Grid>
+
+        <Grid item xs={2} sm={2} md={2} lg={2} xl={2}></Grid>
+        <Grid item xs={2} sm={2} md={2} lg={2} xl={2}></Grid>
+        <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+          <Card align="center" justify="center" alignItems="center">
+            <CardMedia
+              component="img"
+              height="250"
+              image={require("./exit_velocity_vs_launch_angle.png")}
+              style={{
+                width: "auto",
+              }}
+            ></CardMedia>
+          </Card>
+        </Grid>
+        <Grid item xs={4} sm={4} md={4} lg={4} xl={4}>
+          <Card align="center" justify="center" alignItems="center">
+            <CardMedia
+              component="img"
+              height="250"
+              image={require("./launch_angle_vs_hit_distance.png")}
+              style={{
+                width: "auto",
+              }}
+            ></CardMedia>
+          </Card>
+        </Grid>
+        <Grid item xs={2} sm={2} md={2} lg={2} xl={2}></Grid>
       </Grid>
     </div>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
---
+++ visualizer/app.py
@@ -0,0 +1,150 @@
+from flask import Flask, render_template, jsonify, request
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+data = pd.read_csv('BattedBallData.csv')
+data = data.dropna()
+x = data[['LAUNCH_ANGLE', 'EXIT_SPEED', 'EXIT_DIRECTION', 'HIT_DISTANCE', 'HANG_TIME', 'HIT_SPIN_RATE']]
+y = data['PLAY_OUTCOME']
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(x.values, y)
+
+@app.route('/predictions')
+@cross_origin()
+def predictions():
+    predictions = []
+    for index, row in data.iterrows():
+        input = [[row['LAUNCH_ANGLE'], row['EXIT_SPEED'], row['EXIT_DIRECTION'], row['HIT_DISTANCE'], row['HANG_TIME'], row['HIT_SPIN_RATE']]]
+
+        y_pred = knn.predict(input)
+
+        predictions.append({
+            'playId': index,
+            'pitcher': row['PITCHER'],
+            'batter': row['BATTER'],
+            'date': row['GAME_DATE'],
+            'launchAngle': row['LAUNCH_ANGLE'],
+            'exitSpeed': row['EXIT_SPEED'],
+            'exitDirection': row['EXIT_DIRECTION'],
+            'hitDistance': row['HIT_DISTANCE'],
+            'hangTime': row['HANG_TIME'],
+            'hitSpinRate': row['HIT_SPIN_RATE'],
+            'predictedOutcome': y_pred[0],
+            'trueOutcome': row['PLAY_OUTCOME'],
+            'videoLink': row['VIDEO_LINK']
+        })
+
+    return jsonify(predictions)
+
+@app.route('/')
+def display_graphs():
+    data = pd.read_csv('BattedBallData.csv')
+
+    exit_velocity_mean = data['EXIT_SPEED'].mean()
+    exit_velocity_std = data['EXIT_SPEED'].std()
+
+    launch_angle_mean = data['LAUNCH_ANGLE'].mean()
+    launch_angle_std = data['LAUNCH_ANGLE'].std()
+
+    hit_distance_mean = data['HIT_DISTANCE'].mean()
+    hit_distance_std = data['HIT_DISTANCE'].std()
+
+    color_map = {
+        'Out': 'red',
+        'Single': 'blue',
+        'Double': 'green',
+        'Triple': 'purple',
+        'HomeRun': 'orange',
+        'Error': 'gray',
+        'FieldersChoice': 'brown',
+        'Sacrifice': 'yellow',
+        'TriplePlay': 'pink',
+        'Undefined': 'black'
+    }
+
+    plt.figure(figsize=(15, 10))
+
+    plt.subplot(3, 2, 1)
+    for play_outcome in ['Out', 'Single', 'Double', 'Triple', 'HomeRun']:
+        subset_data = data[data['PLAY_OUTCOME'] == play_outcome]
+        plt.scatter(subset_data['EXIT_SPEED'], subset_data['LAUNCH_ANGLE'], label=play_outcome,
+                    color=color_map[play_outcome], alpha=0.5)
+    plt.xlabel('Exit Velocity (mph)')
+    plt.ylabel('Launch Angle (degrees)')
+    plt.title('Exit Velocity vs. Launch Angle')
+    plt.legend()
+    plt.savefig('./static/exit_velocity_vs_launch_angle.png', dpi=300, bbox_inches='tight')
+    plt.clf()
+
+    plt.subplot(3, 2, 2)
+    for play_outcome in ['Out', 'Single', 'Double', 'Triple', 'HomeRun']:
+        subset_data = data[data['PLAY_OUTCOME'] == play_outcome]
+        plt.scatter(subset_data['EXIT_SPEED'], subset_data['HIT_DISTANCE'], label=play_outcome,
+                    color=color_map[play_outcome], alpha=0.5)
+    plt.xlabel('Exit Velocity (mph)')
+    plt.ylabel('Hit Distance (feet)')
+    plt.title('Exit Velocity vs. Hit Distance by Play Outcome')
+    plt.legend()
+    plt.savefig('./static/exit_velocity_vs_hit_distance.png', dpi=300, bbox_inches='tight')
+    plt.clf()
+
+    plt.subplot(3, 2, 3)
+    for play_outcome in ['Out', 'Single', 'Double', 'Triple', 'HomeRun']:
+        subset_data = data[data['PLAY_OUTCOME'] == play_outcome]
+        plt.scatter(subset_data['LAUNCH_ANGLE'], subset_data['HIT_DISTANCE'], label=play_outcome,
+                    color=color_map[play_outcome], alpha=0.5)
+    plt.xlabel('Launch Angle (degrees)')
+    plt.ylabel('Hit Distance (feet)')
+    plt.title('Launch Angle vs. Hit Distance by Play Outcome')
+    plt.legend()
+    plt.savefig('./static/launch_angle_vs_hit_distance.png', dpi=300, bbox_inches='tight')
+    plt.clf()
+
+    plt.subplot(3, 3, 4)
+    plt.hist(data['LAUNCH_ANGLE'], bins=20, edgecolor='black')
+    plt.xlabel('Launch Angle (degrees)')
+    plt.ylabel('Frequency')
+    plt.title('Distribution of Launch Angle')
+    plt.axvline(launch_angle_mean, color='red', linestyle='--', label='Mean Launch Angle')
+    plt.axvline(launch_angle_mean + launch_angle_std, color='blue', linestyle='--', label='Mean + 1 Std. Dev.')
+    plt.axvline(launch_angle_mean - launch_angle_std, color='blue', linestyle='--', label='Mean - 1 Std. Dev.')
+    plt.legend()
+    plt.savefig('./static/launch_angle_distribution', dpi=300, bbox_inches='tight')
+    plt.clf()
+
+    plt.subplot(3, 3, 4)
+    plt.hist(data['EXIT_SPEED'], bins=20, edgecolor='black')
+    plt.xlabel('Exit Speed (mph)')
+    plt.ylabel('Frequency')
+    plt.title('Distribution of Exit Speed')
+    plt.axvline(exit_velocity_mean, color='red', linestyle='--', label='Mean Exit Speed')
+    plt.axvline(exit_velocity_mean + exit_velocity_std, color='blue', linestyle='--', label='Mean + 1 Std. Dev.')
+    plt.axvline(exit_velocity_mean - exit_velocity_std, color='blue', linestyle='--', label='Mean - 1 Std. Dev.')
+    plt.legend()
+    plt.savefig('./static/exit_speed_distribution', dpi=300, bbox_inches='tight')
+    plt.clf()
+
+    plt.subplot(3, 3, 4)
+    plt.hist(data['HIT_DISTANCE'], bins=20, edgecolor='black')
+    plt.xlabel('Hit Distance (ft)')
+    plt.ylabel('Frequency')
+    plt.title('Distribution of Hit Distance')
+    plt.axvline(hit_distance_mean, color='red', linestyle='--', label='Mean Hit Distance')
+    plt.axvline(hit_distance_mean + hit_distance_std, color='blue', linestyle='--', label='Mean + 1 Std. Dev.')
+    plt.axvline(hit_distance_mean - hit_distance_std, color='blue', linestyle='--', label='Mean - 1 Std. Dev.')
+    plt.legend()
+    plt.savefig('./static/hit_distance_distribution', dpi=300, bbox_inches='tight')
+    plt.clf()
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
